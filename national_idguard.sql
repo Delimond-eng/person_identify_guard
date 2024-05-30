50bfb8b603a46f21f5703d64c7621697dcab1b68
unknown <Delimond-eng>
2024-05-30 04:51:28 +0200

    database

--- national_idguard.sql
+++ national_idguard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 30 mai 2024 à 04:43
+-- Généré le : jeu. 30 mai 2024 à 04:51
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.0.28
 
@@ -20,6 +20,235 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `national_idguard`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `chefferies`
+--
+
+CREATE TABLE `chefferies` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `chefferie_libelle` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'actif',
+  `secteur_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `conjoints`
+--
+
+CREATE TABLE `conjoints` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `conjoint_nom` varchar(255) NOT NULL,
+  `conjoint_date_naissance` timestamp NULL DEFAULT NULL,
+  `personne_id` bigint(20) UNSIGNED NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'actif',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `enfants`
+--
+
+CREATE TABLE `enfants` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `eft_nom` varchar(255) NOT NULL,
+  `eft_date_naissance` timestamp NULL DEFAULT NULL,
+  `personne_id` bigint(20) UNSIGNED NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'actif',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etude_titres`
+--
+
+CREATE TABLE `etude_titres` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `titre_libelle` varchar(255) NOT NULL,
+  `titre_date_obtention` timestamp NULL DEFAULT NULL,
+  `personne_id` bigint(20) UNSIGNED NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'actif',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `famille_charges`
+--
+
+CREATE TABLE `famille_charges` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nom_membre_famille` varchar(255) NOT NULL,
+  `lien_parent` varchar(255) NOT NULL,
+  `personne_id` bigint(20) UNSIGNED NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'actif',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2024_05_29_112445_create_personnes_table', 1),
+(6, '2024_05_29_114310_create_conjoints_table', 1),
+(7, '2024_05_29_114841_create_enfants_table', 1),
+(8, '2024_05_29_115330_create_famille_charges_table', 1),
+(9, '2024_05_29_115633_create_etude_titres_table', 1),
+(10, '2024_05_29_115908_create_provinces_table', 1),
+(11, '2024_05_29_120031_create_territoires_table', 1),
+(12, '2024_05_29_120202_create_secteurs_table', 1),
+(13, '2024_05_29_120407_create_chefferies_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personnes`
+--
+
+CREATE TABLE `personnes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `idnat` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `postnom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `date_naissance` timestamp NULL DEFAULT NULL,
+  `sexe` varchar(255) NOT NULL,
+  `etat_civil` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `nbre_personne_famille` varchar(255) DEFAULT NULL,
+  `province_id` bigint(20) UNSIGNED NOT NULL,
+  `territoire_id` bigint(20) UNSIGNED NOT NULL,
+  `secteur_id` bigint(20) UNSIGNED NOT NULL,
+  `chefferie_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `provinces`
+--
+
+CREATE TABLE `provinces` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `province_libelle` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'actif',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `provinces`
+--
+
+INSERT INTO `provinces` (`id`, `province_libelle`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Kinshasa', 'actif', NULL, NULL),
+(2, 'Kongo-Central', 'actif', NULL, NULL),
+(3, 'Kwango', 'actif', NULL, NULL),
+(4, 'Kwilu', 'actif', NULL, NULL),
+(5, 'Mai-Ndombe', 'actif', NULL, NULL),
+(6, 'Equateur', 'actif', NULL, NULL),
+(7, 'Nord-Ubangi', 'actif', NULL, NULL),
+(8, 'Sud-Ubangi', 'actif', NULL, NULL),
+(9, 'Mongala', 'actif', NULL, NULL),
+(10, 'Tshuapa', 'actif', NULL, NULL),
+(11, 'Tshopo', 'actif', NULL, NULL),
+(12, 'Bas-Uele', 'actif', NULL, NULL),
+(13, 'Haut-Uele', 'actif', NULL, NULL),
+(14, 'Ituri', 'actif', NULL, NULL),
+(15, 'Nord-Kivu', 'actif', NULL, NULL),
+(16, 'Sud-Kivu', 'actif', NULL, NULL),
+(17, 'Maniema', 'actif', NULL, NULL),
+(18, 'Haut-Katanga', 'actif', NULL, NULL),
+(19, 'Haut-Lomami', 'actif', NULL, NULL),
+(20, 'Lualaba', 'actif', NULL, NULL),
+(21, 'Tanganyka', 'actif', NULL, NULL),
+(22, 'Lomami', 'actif', NULL, NULL),
+(23, 'Sankuru', 'actif', NULL, NULL),
+(24, 'Kasai Oriental', 'actif', NULL, NULL),
+(25, 'Kasai', 'actif', NULL, NULL),
+(26, 'Tshuapa', 'actif', NULL, NULL),
+(27, 'Kasai Central', 'actif', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -337,9 +566,209 @@ INSERT INTO `secteurs` (`id`, `secteur_libelle`, `status`, `territoire_id`, `cre
 (294, 'Djaga', 'actif', 79, NULL, NULL),
 (295, 'Malolo', 'actif', 79, NULL, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `territoires`
+--
+
+CREATE TABLE `territoires` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `territoire_libelle` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'actif',
+  `province_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `territoires`
+--
+
+INSERT INTO `territoires` (`id`, `territoire_libelle`, `status`, `province_id`, `created_at`, `updated_at`) VALUES
+(1, 'Kasangulu', 'actif', 2, NULL, NULL),
+(2, 'Kimvula', 'actif', 2, NULL, NULL),
+(3, 'Lukula', 'actif', 2, NULL, NULL),
+(4, 'Luozi', 'actif', 2, NULL, NULL),
+(5, 'Madimba', 'actif', 2, NULL, NULL),
+(6, 'Mbanza-Ngungu', 'actif', 2, NULL, NULL),
+(7, 'Moanda', 'actif', 2, NULL, NULL),
+(8, 'Seke-Banza', 'actif', 2, NULL, NULL),
+(9, 'Songololo', 'actif', 2, NULL, NULL),
+(10, 'Tshela', 'actif', 2, NULL, NULL),
+(11, 'Feshi', 'actif', 3, NULL, NULL),
+(12, 'Kahemba', 'actif', 3, NULL, NULL),
+(13, 'Kasongo-Lunda', 'actif', 3, NULL, NULL),
+(14, 'Kenge', 'actif', 3, NULL, NULL),
+(15, 'Popokabaka', 'actif', 3, NULL, NULL),
+(16, 'Bagata', 'actif', 4, NULL, NULL),
+(17, 'Bulungu', 'actif', 4, NULL, NULL),
+(18, 'Gungu', 'actif', 4, NULL, NULL),
+(19, 'Idiofa', 'actif', 4, NULL, NULL),
+(20, 'Masi-Manimba', 'actif', 4, NULL, NULL),
+(21, 'Bolobo', 'actif', 5, NULL, NULL),
+(22, 'Inongo', 'actif', 5, NULL, NULL),
+(23, 'Kiri', 'actif', 5, NULL, NULL),
+(24, 'Kutu', 'actif', 5, NULL, NULL),
+(25, 'Kwamouth', 'actif', 5, NULL, NULL),
+(26, 'Mushie', 'actif', 5, NULL, NULL),
+(27, 'Oshwe', 'actif', 5, NULL, NULL),
+(28, 'Yumbi', 'actif', 5, NULL, NULL),
+(29, 'Basankusu', 'actif', 6, NULL, NULL),
+(30, 'Bikoro', 'actif', 6, NULL, NULL),
+(31, 'Bolomba', 'actif', 6, NULL, NULL),
+(32, 'Bomongo', 'actif', 6, NULL, NULL),
+(33, 'Ingende', 'actif', 6, NULL, NULL),
+(34, 'Lukolela', 'actif', 6, NULL, NULL),
+(35, 'Makanza', 'actif', 6, NULL, NULL),
+(36, 'Bosobolo', 'actif', 7, NULL, NULL),
+(37, 'Businga', 'actif', 7, NULL, NULL),
+(38, 'Mobayi-Mbongo', 'actif', 7, NULL, NULL),
+(39, 'Yakoma', 'actif', 7, NULL, NULL),
+(40, 'Budjala', 'actif', 8, NULL, NULL),
+(41, 'Gemena', 'actif', 8, NULL, NULL),
+(42, 'Kungu', 'actif', 8, NULL, NULL),
+(43, 'Libenge', 'actif', 8, NULL, NULL),
+(44, 'Bongandanga', 'actif', 9, NULL, NULL),
+(45, 'Bumba', 'actif', 9, NULL, NULL),
+(46, 'Lisala', 'actif', 9, NULL, NULL),
+(47, 'Befale', 'actif', 10, NULL, NULL),
+(48, 'Boende', 'actif', 10, NULL, NULL),
+(49, 'Bokungu', 'actif', 10, NULL, NULL),
+(50, 'Djolu', 'actif', 10, NULL, NULL),
+(51, 'Ikela', 'actif', 10, NULL, NULL),
+(52, 'Monkoto', 'actif', 10, NULL, NULL),
+(53, 'Bafwasende', 'actif', 11, NULL, NULL),
+(54, 'Banalia', 'actif', 11, NULL, NULL),
+(55, 'Basoko', 'actif', 11, NULL, NULL),
+(56, 'Isangi', 'actif', 11, NULL, NULL),
+(57, 'Opala', 'actif', 11, NULL, NULL),
+(58, 'Ubundu', 'actif', 11, NULL, NULL),
+(59, 'Yahuma', 'actif', 11, NULL, NULL),
+(60, 'Aketi', 'actif', 12, NULL, NULL),
+(61, 'Ango', 'actif', 12, NULL, NULL),
+(62, 'Bambesa', 'actif', 12, NULL, NULL),
+(63, 'Bondo', 'actif', 12, NULL, NULL),
+(64, 'Buta', 'actif', 12, NULL, NULL),
+(65, 'Poko', 'actif', 12, NULL, NULL),
+(66, 'Dungu', 'actif', 13, NULL, NULL),
+(67, 'Faradje', 'actif', 13, NULL, NULL),
+(68, 'Niangara', 'actif', 13, NULL, NULL),
+(69, 'Kinshasa', 'actif', 1, NULL, NULL),
+(70, 'Barumbu', 'actif', 1, NULL, NULL),
+(71, 'Bumbu', 'actif', 1, NULL, NULL),
+(72, 'Gombe', 'actif', 1, NULL, NULL),
+(73, 'Kalamu', 'actif', 1, NULL, NULL),
+(74, 'Kasa-Vubu', 'actif', 1, NULL, NULL),
+(75, 'Kimbanseke', 'actif', 1, NULL, NULL),
+(76, 'Kinshasa', 'actif', 1, NULL, NULL),
+(77, 'Kintambo', 'actif', 1, NULL, NULL),
+(78, 'Kisenso', 'actif', 1, NULL, NULL),
+(79, 'Lemba', 'actif', 1, NULL, NULL),
+(80, 'Limete', 'actif', 1, NULL, NULL),
+(81, 'Lingwala', 'actif', 1, NULL, NULL),
+(82, 'Makala', 'actif', 1, NULL, NULL),
+(83, 'Maluku', 'actif', 1, NULL, NULL),
+(84, 'Masina', 'actif', 1, NULL, NULL),
+(85, 'Matete', 'actif', 1, NULL, NULL),
+(86, 'Mont Ngafula', 'actif', 1, NULL, NULL),
+(87, 'Ndjili', 'actif', 1, NULL, NULL),
+(88, 'Ngaba', 'actif', 1, NULL, NULL),
+(89, 'Ngaliema', 'actif', 1, NULL, NULL),
+(90, 'Ngiri-Ngiri', 'actif', 1, NULL, NULL),
+(91, 'Nsele', 'actif', 1, NULL, NULL),
+(92, 'Selembao', 'actif', 1, NULL, NULL),
+(93, 'Bandalungwa', 'actif', 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `chefferies`
+--
+ALTER TABLE `chefferies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `conjoints`
+--
+ALTER TABLE `conjoints`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `enfants`
+--
+ALTER TABLE `enfants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `etude_titres`
+--
+ALTER TABLE `etude_titres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Index pour la table `famille_charges`
+--
+ALTER TABLE `famille_charges`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Index pour la table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Index pour la table `personnes`
+--
+ALTER TABLE `personnes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `provinces`
+--
+ALTER TABLE `provinces`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `secteurs`
@@ -348,14 +777,99 @@ ALTER TABLE `secteurs`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `territoires`
+--
+ALTER TABLE `territoires`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `chefferies`
+--
+ALTER TABLE `chefferies`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `conjoints`
+--
+ALTER TABLE `conjoints`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `enfants`
+--
+ALTER TABLE `enfants`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `etude_titres`
+--
+ALTER TABLE `etude_titres`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `famille_charges`
+--
+ALTER TABLE `famille_charges`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT pour la table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `personnes`
+--
+ALTER TABLE `personnes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `provinces`
+--
+ALTER TABLE `provinces`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT pour la table `secteurs`
 --
 ALTER TABLE `secteurs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=296;
+
+--
+-- AUTO_INCREMENT pour la table `territoires`
+--
+ALTER TABLE `territoires`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
